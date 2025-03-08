3730cdf5cfeb3d1a2ecc25bc3db7fbdadb67da7c
aadivik <109094673+aadivik@users.noreply.github.com>
2025-03-08 15:37:17 +0000

    commit 03

---
+++ 01_basics/03_conversionOperation.js
@@ -0,0 +1,28 @@
+let score = "33abc" //33abc - Nan after conversion 
+// //null - 0 //undefine - Nan. // True - 1 and False - 0
+
+console.log(typeof score);
+
+
+let valueInNumber = Number(score)
+
+console.log(typeof(valueInNumber));
+
+console.log(valueInNumber);
+
+let isLoggedIn = "H"
+
+// 1 - true 2- false
+// empty string - false and string - true
+// Start with capital number for conversion
+
+let booleanIsLoggedIn = Boolean(isLoggedIn)
+
+console.log(booleanIsLoggedIn);
+
+let someNumber= 33
+let stringNumber = String(someNumber)
+console.log(stringNumber);
+console.log(typeof(stringNumber));
+
+
