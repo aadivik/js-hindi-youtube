99efd152202df273dca3cded75b29564a3b6f5a2
aadivik <109094673+aadivik@users.noreply.github.com>
2025-03-08 15:06:39 +0000

    commit datatypes

---
+++ 01_basics/02_datatypes.js
@@ -0,0 +1,31 @@
+"use strict"; // treat all JS as newer version
+
+// alert(3+3) // we are using nodejs and not browser
+
+// number => 2 to power 53
+// bigint 
+// string => ""
+// boolean => true/False
+
+// null standalone value
+
+//undefined => like below
+let state 
+
+let state3 = null
+
+// symbol => uniqueness ke lie use hota hai
+
+// objext
+
+let age = 54
+console.log(typeof age)
+console.log(typeof undefined) // undefined type hai
+console.log(typeof null) // null is object type interview ques
+
+
+
+
+
+
+
